a4963058799935ef80a9f5890ad2c4ed8eec7d08
mateusjmaf <mateus.jmaf@gmail.com>
2018-05-07 21:59:50 -0300

    Removed unnecessary property on card component

--- src/app/product/product.object.ts
+++ src/app/product/product.object.ts
@@ -4,20 +4,17 @@ export class Product{
 
   idProduto: number;
 	nome: string;
-	codigo: number;
 	descricao: string;
 	valor: number;
 
   constructor(
     id?: number,
     nome?: string,
-    codigo?: number,
     descricao?: string,
     valor?: number
   ){
     this.idProduto = id;
     this.nome = nome;
-    this.codigo = codigo;
     this.descricao = descricao;
     this.valor = valor;
   }
