89d95ceebe546de8566aaa15158de976f17128a3
mateusjmaf <mateus.jmaf@gmail.com>
2018-05-16 22:07:24 -0300

    started code of th cash flow component

--- src/app/cashFlow/cash-flow.component.ts
+++ src/app/cashFlow/cash-flow.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CashFlow } from './cash-flow.object';
+
+import { ServerHttpService } from '../../service/server.http.service';
 
 @Component({
   selector: 'app-cash-flow',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashFlowComponent implements OnInit {
 
-  constructor() { }
+  cashFlow: CashFlow;
+  clientList: CashFlow[];
+  clientSearchValue: string = '';
+  restRoute: string = 'clienteRest';
 
+  constructor(private serverHttp: ServerHttpService) { }
+  
   ngOnInit() {
+    this.cashFlow = new CashFlow();
+    this.cashFlow.tipoMovimento = 'outflow';
+    this.searchClient();
   }
+  
+  addCategoria() {
+
+  }
+
+  onSubmit() {
+    if(this.cashFlow.idRegistro) {
+      return this.serverHttp.update(this.cashFlow, this.restRoute+'/editarCliente').subscribe(response => {
+        alert(response);
+        this.resetForm();
+      })
+    } else {
+      return this.serverHttp.create(this.cashFlow, this.restRoute+'/criarCliente').subscribe(
+        response => { 
+          this.searchClient();
+          this.resetForm();
+        }
+      )
+    }
+  }
+
+  deleteClient(id: number) {
+    return this.serverHttp.delete(id, this.restRoute+'/deletarCliente').subscribe(
+      response => { 
+        this.searchClient();
+      }
+    )
+  }
+
+  searchClient() {
+    return this.serverHttp.readByName(this.clientSearchValue, this.restRoute+'/buscarClientesPorNome').subscribe(response => {
+      response.length > 0 ? this.clientList = response : this.clientList = undefined;
+    })
+  }
+
+  editClient(clientParam: CashFlow) {
+    this.cashFlow = clientParam;
+  }
+
+  resetForm() {
+    this.cashFlow = new CashFlow();
+  }
+
 
 }
--- src/app/cashFlow/cash-flow.object.ts
+++ src/app/cashFlow/cash-flow.object.ts
@@ -1,11 +1,30 @@
 import { Reserve } from "../reserve/reserve.object";
 
 export class CashFlow {
-  idRegitro: number;
-	tipoMovimento: string;
-	reserva: Reserve;
-	formaPagamento: string;
-	nParcelas;
+  idRegistro: number;
 	dataRegistro: Date;
+	formaPagamento: string;
+	nParcelas: number;
+	reserva: Reserve;
+	tipoMovimento: string;
 	valor: number;
+
+	constructor (
+		idRegistro?: number,
+		dataRegistro?: Date,
+		formaPagamento?: string,
+		nParcelas?: number,
+		reserva?: Reserve,
+		tipoMovimento?: string,
+		valor?: number,) {
+
+			this.idRegistro = idRegistro;
+			this.dataRegistro = dataRegistro;
+			this.formaPagamento = formaPagamento;
+			this.nParcelas = nParcelas;
+			this.reserva = reserva;
+			this.tipoMovimento = tipoMovimento;
+			this.valor = valor;
+	}
+	
 }
