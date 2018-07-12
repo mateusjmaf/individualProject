a2d66dd7812d69ce725c9502a5a1a1cb31ae45bc
mateusjmaf <mateus.jmaf@gmail.com>
2018-07-11 22:03:25 -0300

    Improvements in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { AuthService } from './auth.service';
+import { User } from './user.object';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  private user = new User();
 
-  ngOnInit() {
+  constructor(private authService: AuthService) { }
+
+  onSubmit() {
+    this.authService.loginRequest(this.user);
   }
 
 }
